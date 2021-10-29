34827295b73c70b7d65132b3188af0bdf603711f
Yshi29 <yshi29@mylanagra.ca>
2021-10-29 15:19:04 -0700

    Create coming soon page

---
+++ Desktop/quality-dashboard/src/App.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+        fontFamily: "roboto",
+      }}
+    >
+      <h1
+        style={{
+          color: "#6ec177",
+          margin: 0,
+        }}
+      >
+        Wah Gwan, Soon Coming
+      </h1>
+      <p>What's up, Coming soon</p>
+    </div>
+  );
+};
+
+export default App;
