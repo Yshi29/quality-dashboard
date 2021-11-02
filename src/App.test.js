61efcdc01262b351c9cf22593657034556003b8b
Yshi29 <yshi29@mylanagra.ca>
2021-11-02 13:06:06 -0700

    update

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,2 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
-
-test("renders coming soon text", () => {
-  render(<App />);
-  const h1Element = screen.getByText(/Wah Gwan, Soon Coming/i);
-  expect(h1Element).toBeInTheDocument();
-});
